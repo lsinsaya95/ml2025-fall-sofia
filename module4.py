0d4f985a80af4af257b8b032655af0d4c929e274
lsinsaya95 <lourdessinsaya95@gmail.com>
2025-10-23 15:45:58 -0700

    Add files via upload
    The program asks the user for input N (positive integer) and reads it
    
    Then the program asks the user to provide N numbers (one by one) and reads all of them (again, one by one)
    
    In the end, the program asks the user for input X (integer) and outputs: "-1" if there were no such X among N read numbers, or the index (from 1 to N) of this X if the user inputed it before.

---
+++ module4.py
@@ -0,0 +1,15 @@
+# module4.py
+
+N = int(input("Enter how many positive numbers: "))
+
+numbers = []
+for i in range(N):
+    num = int(input("Enter a number: "))
+    numbers.append(num)
+
+X = int(input("Enter the number to search: "))
+
+if X in numbers:
+    print( 1)
+else:
+    print(-1)
